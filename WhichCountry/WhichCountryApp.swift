06dcefc029161aa0050aed13e2919335e3e93935
Can Kaynar <cannkaynarr@outlook.com>
2024-05-26 02:03:31 +0300

    Initial Commit

---
+++ WhichCountry/WhichCountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhichCountryApp.swift
+//  WhichCountry
+//
+//  Created by Can on 26.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct WhichCountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
